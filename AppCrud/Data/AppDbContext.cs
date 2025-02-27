4aa3ccd79ff2f185d8c867d001f670e47e1f88c8
Felipe Acosta Coronado <felipeacostacoronado@gmail.com>
2025-02-27 01:03:13 -0600

    Agregar archivos de proyecto.

---
+++ AppCrud/Data/AppDbContext.cs
@@ -0,0 +1,49 @@
+﻿using AppCrud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCrud.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Empleado> Empleados { get; set; }
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Empleado>(tb =>
+            {
+                // Propiedades de la columna IdEmpleado
+                tb.HasKey(col => col.IdEmpleado);
+                tb.Property(col => col.IdEmpleado)
+                .UseIdentityColumn()
+                .ValueGeneratedOnAdd();
+
+                // Propiedades de la columna Nombre
+                tb.Property(col => col.Nombre).HasMaxLength(50).IsRequired();
+
+                // Propiedades de la columna Apellido
+                tb.Property(col => col.Apellido).HasMaxLength(50).IsRequired();
+
+                // Propiedades de la columna Cargo
+                tb.Property(col => col.Cargo).HasMaxLength(50).IsRequired();
+
+                // Propiedades de la columna Direccion
+                tb.Property(col => col.Direccion).HasMaxLength(100);
+
+                // Propiedades de la columna Telefono
+                tb.Property(col => col.Telefono).IsRequired();
+
+                // Propiedades de la columna Email
+                tb.Property(col => col.Email).HasMaxLength(50).IsRequired();
+
+                // Propiedades de la columna Activo
+                tb.Property(col => col.Activo).IsRequired();
+            });
+
+            modelBuilder.Entity<Empleado>().ToTable("Empleados");
+        }
+    }
+}
---
+++ AppCrud/Models/Empleado.cs
@@ -0,0 +1,14 @@
+﻿namespace AppCrud.Models
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cargo { get; set; }
+        public string Direccion { get; set; }
+        public int Telefono { get; set; }
+        public string Email { get; set; }
+        public bool Activo { get; set; }
+    }
+}
